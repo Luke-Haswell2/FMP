e30549cd2231ff3b203d1a2e2bafd15fbba22504
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2024-04-26 14:50:18 +0100

    Door and Lightswitch

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,17 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Door : MonoBehaviour
 {
+    public Animator door;
+
+    public GameObject doorText;
+
+    public AudioSource doorSound;
+
+    public bool inReach;
 
     void Start()
     {
-        
+        inReach = false;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && inReach)
+        {
+            DoorOpens();
+        }
+        else
+        {
+            DoorCloses();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            doorText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            doorText.SetActive(false);
+        }
+    }
+
+    void DoorOpens()
+    {
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+        doorSound.Play();
+    }
+
+    void DoorCloses()
+    {
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
     }
 }
--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -1,17 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class LightSwitch : MonoBehaviour
 {
+    public GameObject onSwitch;
+    public GameObject offSwitch;
+
+    public GameObject lightText;
+
+    public GameObject lightObject;
+
+    public AudioSource switchSound;
+
+    public bool lightsAreOn;
+    public bool lightsAreOff;
+    public bool inReach;
 
     void Start()
     {
-        
+        inReach = false;
+        lightsAreOn = false;
+        lightsAreOff = true;
+        onSwitch.SetActive(false);
+        offSwitch.SetActive(true);
+        lightObject.SetActive(false);
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && inReach && lightsAreOn)
+        {
+            lightObject.SetActive(false);
+            onSwitch.SetActive(false);
+            offSwitch.SetActive(true);
+            switchSound.Play();
+            lightsAreOff = true;
+            lightsAreOn = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Mouse0) && inReach && lightsAreOff)
+        {
+            lightObject.SetActive(true);
+            onSwitch.SetActive(true);
+            offSwitch.SetActive(false);
+            switchSound.Play();
+            lightsAreOff = false;
+            lightsAreOn = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            lightText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            lightText.SetActive(false);
+        }
     }
 }
