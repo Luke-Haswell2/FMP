417755d802c5652f2d55bacfd2342b8ac3ea6377
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2024-05-02 10:28:08 +0100

    Update ScenarioLoaderMenu.cs

--- Assets/Scripts/ScenarioLoaderMenu.cs
+++ Assets/Scripts/ScenarioLoaderMenu.cs
@@ -9,25 +9,25 @@ public class ScenarioLoaderMenu : MonoBehaviour
     public void LoadAlpha()
     {
         Time.timeScale = 1f;
-        //SceneManager.LoadScene("Scenario Alpha");
+        SceneManager.LoadScene("Scenario Alpha");
         print("Scene load alpha");
     }
     public void LoadBravo()
     {
         Time.timeScale = 1f;
-        //SceneManager.LoadScene("Scenario Bravo");
+        SceneManager.LoadScene("Scenario Bravo");
         print("Scene load bravo");
     }
     public void LoadCharlie()
     {
         Time.timeScale = 1f;
-        //SceneManager.LoadScene("Scenario Charlie");
+        SceneManager.LoadScene("Scenario Charlie");
         print("Scene load charlie");
     }
     public void LoadDelta()
     {
         Time.timeScale = 1f;
-        //SceneManager.LoadScene("Scenario Delta");
+        SceneManager.LoadScene("Scenario Delta");
         print("Scene load delta");
     }
     public void Resume()
